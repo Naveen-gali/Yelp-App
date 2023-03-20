72050e56580439b10590a46c37adbd1d0d3970e8
Naveen-gali <naveen.gali@mutualmobile.com>
2023-03-20 17:47:00 +0530

    Splash Screen Hiding Logic Changed

--- App.tsx
+++ App.tsx
@@ -1,24 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, {useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {RootNavigator} from './src/navigation/RootNavigator';
 import RNBootsplash from 'react-native-bootsplash';
 
 function App(): JSX.Element {
-  useEffect(() => {
-    RNBootsplash.hide({
-      fade: true,
-    });
-  });
-
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      onReady={() =>
+        RNBootsplash.hide({
+          duration: 500,
+          fade: true,
+        })
+      }>
       <RootNavigator />
     </NavigationContainer>
   );
