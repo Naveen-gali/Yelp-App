4d202de4e3c87ccd9be743fc0e81e5454b936dab
Naveen-gali <naveen.gali@mutualmobile.com>
2023-03-21 12:18:40 +0530

    Merge Fixes

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Delivery from '../../assets/icons/delivery_bike.svg';
+import Restaurant from '../../assets/icons/restaurant-waiter.svg';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Delivery width={100} height={100} />
+      <Restaurant width={100} height={100} />
     </View>
   );
 };
