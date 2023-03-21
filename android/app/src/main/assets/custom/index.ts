6b9f528a99bbb639d9a53f6ab936b2f0f2d37c05
Naveen-gali <naveen.gali@mutualmobile.com>
2023-03-21 12:35:56 +0530

    Fonts RElated comments Fixed

--- android/app/src/main/assets/custom/index.ts
+++ android/app/src/main/assets/custom/index.ts
@@ -1,1 +1,8 @@
-export * from './';
+export const Fonts = {
+  Bold: 'Poppins-Bold',
+  Light: 'Poppins-Light',
+  Medium: 'Poppins-Medium',
+  Regular: 'Poppins-Regular',
+  SemiBold: 'Poppins-SemiBold',
+  RM_Regular: 'RumRaisin-Regular',
+};
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Fonts} from '../../assets';
 import Delivery from '../../assets/icons/delivery_bike.svg';
 import Restaurant from '../../assets/icons/restaurant-waiter.svg';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text
+        style={{
+          fontFamily: Fonts.RM_Regular,
+        }}>
+        Home
+      </Text>
       <Delivery width={100} height={100} />
       <Restaurant width={100} height={100} />
     </View>
