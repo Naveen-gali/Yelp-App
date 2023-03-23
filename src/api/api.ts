ce54e715aa4a72b411e4ff9ff239c3d8cafed74f
Naveen-gali <naveen.gali@mutualmobile.com>
2023-03-23 10:23:57 +0530

    Axios Setup Completed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,45 @@
-export {};
+import axios from 'axios';
+import Config from 'react-native-config';
+
+const YelpApi = axios.create({
+  baseURL: Config.API_URL,
+});
+
+YelpApi.interceptors.request.use(
+  function (config) {
+    config.params = {
+      accept: 'application/json',
+      ...config.params,
+    };
+    config.headers.Authorization = `Bearer ${Config.TOKEN}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+YelpApi.interceptors.response.use(
+  function (response) {
+    return response.data;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+export function GetApi<T>(
+  location: string,
+  sort: string,
+  limit: number,
+): Promise<T> {
+  return YelpApi.get(
+    `/search?location=${location}&sort_by=${sort}&limit=${limit}`,
+  )
+    .then(response => {
+      return response;
+    })
+    .catch(err => {
+      return err;
+    });
+}
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import Config from 'react-native-config';
+import {GetApi} from '../../api';
 import {PrimaryFonts} from '../../assets';
 import Delivery from '../../assets/icons/delivery_bike.svg';
 import Restaurant from '../../assets/icons/restaurant-waiter.svg';
 
 const HomeScreen = () => {
+  useEffect(() => {
+    GetApi('NYC', 'best_match', 20)
+      .then(res => {
+        console.log('RES ', res);
+      })
+      .catch(er => {
+        console.log('ERR ', er);
+      });
+  }, []);
+
   return (
     <View>
       <Text
@@ -13,6 +25,7 @@ const HomeScreen = () => {
         }}>
         Home
       </Text>
+      <Text>{Config.API_URL}</Text>
       <Delivery width={100} height={100} />
       <Restaurant width={100} height={100} />
     </View>
