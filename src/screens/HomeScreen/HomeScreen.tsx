e12d829a65ca7867b4a462f513f6f10e51f10985
Naveen-gali <122969303+Naveen-gali@users.noreply.github.com>
2023-03-29 12:30:15 +0530

    Custom SVGS added and icomoon updated with all the necessary icons (#24)

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ScrollView, Text} from 'react-native';
 import {PrimaryFonts, SVGS} from '../../assets';
 import {CustomIcon, CustomIconNames} from '../../components';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <ScrollView>
       <Text
         style={{
           fontFamily: PrimaryFonts.SemiBold,
@@ -15,8 +15,24 @@ const HomeScreen = () => {
       <CustomIcon name={CustomIconNames.Delivery} size={100} />
       <CustomIcon name={CustomIconNames.RestaurantPlate} size={100} />
       <CustomIcon name={CustomIconNames.AmazonPay} size={100} />
+      <CustomIcon name={CustomIconNames.Arts} size={100} />
+      <CustomIcon name={CustomIconNames.Education} size={100} />
+      <CustomIcon name={CustomIconNames.Events} size={100} />
+      <CustomIcon name={CustomIconNames.Finance} size={100} />
+      <CustomIcon name={CustomIconNames.Government} size={100} />
+      <CustomIcon name={CustomIconNames.Pets} size={100} />
+      <CustomIcon name={CustomIconNames.Professional} size={100} />
+      <CustomIcon name={CustomIconNames.Religious} size={100} />
       <SVGS.YelpLogo width={200} height={200} />
-    </View>
+      <Text>CAtegory SVGS</Text>
+      <SVGS.Active width={100} height={100} />
+      <SVGS.Automotive width={100} height={100} />
+      <SVGS.Barber width={100} height={100} />
+      <SVGS.HomeServices width={100} height={100} />
+      <SVGS.NightLife width={100} height={100} />
+      <SVGS.Restaurant width={100} height={100} />
+      <SVGS.Shopping width={100} height={100} />
+    </ScrollView>
   );
 };
 
