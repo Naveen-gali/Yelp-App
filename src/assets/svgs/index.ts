18ba9dda8f1235160e7c8fe5c0c87aea23edaf53
Naveen-gali <naveen.gali@mutualmobile.com>
2023-03-29 09:32:26 +0530

    Custom SVGS added and icomoon updated with all the necessary icons

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,7 +1,21 @@
 import YelpLogo from './yelp.svg';
+import ActiveSVG from './active.svg';
+import AutomotiveSVG from './automotives.svg';
+import BarberSVG from './barbers.svg';
+import HomeServicesSVG from './homeservices.svg';
+import NightLifeSVG from './nightlife.svg';
+import RestaurantSVG from './restaurants.svg';
+import ShoppingSVG from './shopping.svg';
 
 const SVGS = {
   YelpLogo: YelpLogo,
+  Active: ActiveSVG,
+  Automotive: AutomotiveSVG,
+  Barber: BarberSVG,
+  HomeServices: HomeServicesSVG,
+  NightLife: NightLifeSVG,
+  Restaurant: RestaurantSVG,
+  Shopping: ShoppingSVG,
 };
 
 export {SVGS};
--- src/components/CustomIcon/CustomIconTypes.ts
+++ src/components/CustomIcon/CustomIconTypes.ts
@@ -8,4 +8,12 @@ export enum CustomIconNames {
   Delivery = 'delivery',
   RestaurantPlate = 'restaurant-plate',
   AmazonPay = 'amazonpay',
+  Religious = 'religious',
+  Events = 'events',
+  Finance = 'financial_services',
+  Professional = 'professional_services',
+  Arts = 'arts_and_entertainment',
+  Government = 'government',
+  Education = 'education',
+  Pets = 'pets',
 }
