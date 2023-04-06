ae397b2625a307dfb2bda89e234535e86bce4905
Naveen-gali <naveen.gali@mutualmobile.com>
2023-04-06 15:38:13 +0530

    Cleared Preview.tsx

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,11 +1,15 @@
 import {addDecorator} from '@storybook/react-native';
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 const Decorator = (Story: any) => (
-  <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
+  <View style={styles.decoratorStyle}>
     <Story />
   </View>
 );
+
+const styles = StyleSheet.create({
+  decoratorStyle: {alignItems: 'center', justifyContent: 'center', flex: 1},
+});
 
 export {Decorator};
