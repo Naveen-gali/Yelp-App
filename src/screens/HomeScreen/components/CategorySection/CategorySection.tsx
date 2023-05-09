f13981554db38da6cfc2030577d1aa5c4c3ae35c
Naveen-gali <naveen.gali@mutualmobile.com>
2023-05-09 07:47:12 +0530

    Render Item Extracted and Item name changed to title

--- src/screens/HomeScreen/components/CategorySection/CategorySection.tsx
+++ src/screens/HomeScreen/components/CategorySection/CategorySection.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, ListRenderItemInfo, StyleSheet, View} from 'react-native';
 import {FeaturedCategoriesData} from '../../../../assets/data';
 import {CategoryItem} from '../../../../components';
 import {horizontalScale} from '../../../../utils';
+import {CategorySectionItem} from './CategorySection.types';
 
 const CategorySection = () => {
   const horizontalLine = () => {
     return <View style={styles.horizontalLine} />;
   };
+
+  const renderItem = (props: ListRenderItemInfo<CategorySectionItem>) => {
+    const {item, index} = props;
+    return <CategoryItem key={index} icon={item.logo} title={item.title} />;
+  };
+
   return (
     <FlatList
       data={FeaturedCategoriesData}
-      renderItem={({item, index}) => (
-        <CategoryItem key={index} icon={item.logo} title={item.name} />
-      )}
+      renderItem={renderItem}
       showsHorizontalScrollIndicator={false}
       numColumns={4}
       ListFooterComponent={horizontalLine}
---
+++ src/screens/HomeScreen/components/CategorySection/CategorySection.types.ts
@@ -0,0 +1,6 @@
+import {LocalizedLanguages} from '../../../../i18n';
+
+export type CategorySectionItem = {
+  title: LocalizedLanguages;
+  logo: string;
+};
