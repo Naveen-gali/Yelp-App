abd373c5f1030dff7e69dd73fcdafed77e667831
Naveen-gali <naveen.gali@mutualmobile.com>
2023-06-12 15:52:47 +0530

    Screen Tracking added using firebase Analytics and React Navigation.

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,22 +1,46 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from '@react-navigation/native';
 import {observer} from 'mobx-react-lite';
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import {useColorScheme} from 'react-native';
 import {RootStoreContext} from '../models';
 import {DarkTheme, LightTheme} from '../theme';
 import {AuthStackNavigator} from './AuthStack';
 import {BottomTabNavigator} from './BottomTabs';
+import analytics from '@react-native-firebase/analytics';
 
 const RootNavigator = observer(() => {
   const theme = useColorScheme();
   const {auth} = useContext(RootStoreContext);
+
+  const routeNameRef = useRef<string>();
+  const navigationRef = useNavigationContainerRef();
 
   useEffect(() => {
     auth.checkSignin();
   }, [auth]);
 
   return (
-    <NavigationContainer theme={theme === 'dark' ? DarkTheme : LightTheme}>
+    <NavigationContainer
+      theme={theme === 'dark' ? DarkTheme : LightTheme}
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current?.getCurrentRoute()?.name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current?.getCurrentRoute()?.name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+      }}>
       {auth.isAuthenticated ? <BottomTabNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
