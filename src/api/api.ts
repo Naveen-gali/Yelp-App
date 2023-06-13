80f6c3edd90609427810b0412935001b1048c78d
Naveen-gali <naveen.gali@mutualmobile.com>
2023-06-13 09:16:04 +0530

    Performance Monitoring Added for axios requests waiting for responses in dashboard.

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,22 +3,51 @@ import {Alert} from 'react-native';
 import Config from 'react-native-config';
 import {Strings} from '../i18n';
 import {SuccessResponse} from './api.types';
+import perf from '@react-native-firebase/perf';
 
 const YelpApi = axios.create({
   baseURL: Config.API_URL,
 });
 
 YelpApi.interceptors.request.use(
-  function (config) {
-    config.params = {
-      accept: 'application/json',
-      ...config.params,
-    };
-    config.headers.Authorization = `Bearer ${Config.TOKEN}`;
-    return config;
+  async function (config) {
+    try {
+      config.params = {
+        accept: 'application/json',
+        ...config.params,
+      };
+      config.headers.Authorization = `Bearer ${Config.TOKEN}`;
+      const httpMetric = await perf().startTrace('axios-request');
+
+      httpMetric.start();
+    } finally {
+      return config;
+    }
   },
   function (error) {
     throw error;
+  },
+);
+
+axios.interceptors.response.use(
+  async function (response) {
+    try {
+      const httpMetric = await perf().startTrace('axios-response');
+      httpMetric.putMetric('http_code', response.status);
+      httpMetric.stop();
+    } finally {
+      return response;
+    }
+  },
+  async function (error) {
+    try {
+      const {httpMetric} = error.config.metadata;
+      httpMetric.setHttpResponseCode(error.response.status);
+      httpMetric.setResponseContentType(error.response.headers['Content-Type']);
+      await httpMetric.stop();
+    } finally {
+      return error;
+    }
   },
 );
 
