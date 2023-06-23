6855c2e189c496c5c3b9f1723d60a506d223eaf5
Naveen-gali <naveen.gali@mutualmobile.com>
2023-06-23 06:34:15 +0530

    requestPermission Service Added.

--- src/services/Permission/PermissionService.ts
+++ src/services/Permission/PermissionService.ts
@@ -11,16 +11,17 @@ import {DeviceUtils} from '../../utils';
 import {RequiredPermissions} from './PermissionService.types';
 
 const getPermissionBasedOnPlatform = (permission: RequiredPermissions) => {
-  if (permission === RequiredPermissions.Camera) {
-    return DeviceUtils.isIos
-      ? PERMISSIONS.IOS.CAMERA
-      : PERMISSIONS.ANDROID.CAMERA;
-  } else if (permission === RequiredPermissions.PhotoLibrary) {
-    return DeviceUtils.isIos
-      ? PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY
-      : PERMISSIONS.ANDROID.READ_MEDIA_IMAGES;
+  switch (permission) {
+    case RequiredPermissions.Camera:
+      return DeviceUtils.isIos
+        ? PERMISSIONS.IOS.CAMERA
+        : PERMISSIONS.ANDROID.CAMERA;
+
+    case RequiredPermissions.PhotoLibrary:
+      return DeviceUtils.isIos
+        ? PERMISSIONS.IOS.PHOTO_LIBRARY_ADD_ONLY
+        : PERMISSIONS.ANDROID.READ_MEDIA_IMAGES;
   }
-  throw new Error(Strings.locationService.invalidPermission);
 };
 
 const showAlert = (
@@ -38,100 +39,144 @@ let permissionDeniedMap: Record<RequiredPermissions, boolean> = {
 };
 
 // Called when the Result is RESULTS.UNAVAILABLE
-function resultUnavailable(): void {
-  return showAlert(
-    Strings.locationService.notAvailable,
-    Strings.locationService.notAvailable,
-  );
+async function resultUnavailable(): Promise<boolean> {
+  return new Promise(resolve => {
+    return showAlert(
+      Strings.locationService.notAvailable,
+      Strings.locationService.notAvailable,
+      [
+        {
+          text: Strings.locationService.cancel,
+          onPress: () => resolve(false),
+        },
+      ],
+    );
+  });
 }
 
 // Called when the Result is RESULTS.BLOCKED
-function resultsBlocked() {
-  return showAlert(
-    Strings.locationService.blocked,
-    Strings.locationService.blockedPermission,
-    [
-      {
-        text: Strings.locationService.goToSettings,
-        onPress: async () => await openSettings(),
-      },
-      {
-        text: Strings.locationService.cancel,
-      },
-    ],
-  );
+async function resultsBlocked(): Promise<boolean> {
+  return new Promise(resolve => {
+    showAlert(
+      Strings.locationService.blocked,
+      Strings.locationService.blockedPermission,
+      [
+        {
+          text: Strings.locationService.goToSettings,
+          onPress: async () => {
+            await openSettings();
+            resolve(true);
+          },
+        },
+        {
+          text: Strings.locationService.cancel,
+          onPress: () => resolve(false),
+        },
+      ],
+    );
+  });
 }
 
 // Called when the Result is RESULTS.DENIED
-function resultsDenied() {
-  return showAlert(
-    Strings.locationService.denied,
-    Strings.locationService.deniedDescription,
-    [
-      {
-        text: Strings.locationService.goToSettings,
-        onPress: async () => await openSettings(),
-      },
-      {
-        text: Strings.locationService.cancel,
-      },
-    ],
-  );
+async function resultsDenied(): Promise<boolean> {
+  return new Promise(resolve => {
+    showAlert(
+      Strings.locationService.denied,
+      Strings.locationService.deniedDescription,
+      [
+        {
+          text: Strings.locationService.goToSettings,
+          onPress: async () => {
+            await openSettings();
+            resolve(true);
+          },
+        },
+        {
+          text: Strings.locationService.cancel,
+          onPress: () => resolve(false),
+        },
+      ],
+    );
+  });
 }
 
 // Called when the Result is returned default.
 function defaultCase() {
-  return showAlert(
-    Strings.locationService.noPermission,
-    Strings.locationService.noPermission,
-    [
-      {
-        text: Strings.locationService.goToSettings,
-        onPress: async () => await openSettings(),
-      },
-      {
-        text: Strings.locationService.cancel,
-      },
-    ],
-  );
+  return new Promise(resolve => {
+    showAlert(
+      Strings.locationService.noPermission,
+      Strings.locationService.noPermission,
+      [
+        {
+          text: Strings.locationService.goToSettings,
+          onPress: async () => {
+            await openSettings();
+            resolve(true);
+          },
+        },
+        {
+          text: Strings.locationService.cancel,
+          onPress: () => resolve(false),
+        },
+      ],
+    );
+  });
 }
+
+const requestPermission = async (permission: RequiredPermissions) => {
+  const requestStatus = await request(getPermissionBasedOnPlatform(permission));
+
+  console.log('REQ STATUS :_ ', requestStatus);
+
+  switch (requestStatus) {
+    case RESULTS.UNAVAILABLE:
+      await resultUnavailable();
+      break;
+    case RESULTS.DENIED:
+      if (permissionDeniedMap[permission]) {
+        await resultsDenied();
+      }
+      permissionDeniedMap[permission] = true;
+      break;
+    case RESULTS.LIMITED:
+    case RESULTS.GRANTED:
+      return;
+    case RESULTS.BLOCKED:
+      await resultsBlocked();
+      break;
+    default:
+      await defaultCase();
+      break;
+  }
+};
 
 const checkPermission = async (permission: RequiredPermissions) => {
   const permissionStatus = await check(
     getPermissionBasedOnPlatform(permission),
   );
   let hasPermission = false;
+  console.log('PS :_ ', permissionStatus);
 
   switch (permissionStatus) {
     case RESULTS.UNAVAILABLE:
-      resultUnavailable();
+      await resultUnavailable();
       break;
     case RESULTS.DENIED:
-      const requestStatus = await request(
-        getPermissionBasedOnPlatform(permission),
-      );
-
-      if (requestStatus === RESULTS.GRANTED) {
-        hasPermission = true;
-      } else {
-        if (permissionDeniedMap[permission]) {
-          resultsDenied();
-        } else {
-          permissionDeniedMap[permission] = true;
-        }
-      }
+      await requestPermission(permission);
       break;
     case RESULTS.LIMITED:
     case RESULTS.GRANTED:
       hasPermission = true;
       break;
     case RESULTS.BLOCKED:
-      resultsBlocked();
+      await resultsBlocked();
       break;
     default:
-      defaultCase();
+      await defaultCase();
       break;
   }
+  console.log('END');
+
   return hasPermission;
 };
 
